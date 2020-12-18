a7afa87edff6db2ddd162f74a2f89981c36604f6
Lyubov <miloslubov@gmail.com>
2020-12-18 10:06:05 +0300

    - removed clone-mrob.sh

--- scripts/clone-mrob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ~/soft
-cd ~/soft
-
-git clone --recursive --branch feature/wheel https://github.com/miloserdova-l/mrob
-cd ~/soft/mrob
-git submodule update --recursive 
-mkdir build
