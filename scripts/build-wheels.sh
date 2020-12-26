25a0015767cb20c39e76c9080a54592bc0dddea9
Lyubov <miloslubov@gmail.com>
2020-12-26 15:54:21 +0300

    - new path to building wheel

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -2,27 +2,28 @@
 
 LATEST=""
 
-mkdir -p ~/soft/wheel/mrob
-mkdir ~/soft/wheel/dist
 
-cp ~/soft/mrob/README.md ~/soft/wheel/README.md 
-cp ~/soft/mrob/LICENSE ~/soft/wheel/LICENSE
-cp ~/soft/mrob/__init__.py ~/soft/wheel/mrob/__init__.py 
-cp ~/soft/mrob/pyproject.toml ~/soft/wheel/pyproject.toml
-cp ~/soft/mrob/setup.cfg ~/soft/wheel/setup.cfg
+mkdir ./build
+mkdir ./dist
+mkdir ./mrob
+
+cp ./__init__.py ./mrob/__init__.py 
+
+cd ./build
+
+NUMPROC=$(grep -Ec '^processor\s+:' /proc/cpuinfo)
 
 for PYBIN in /opt/python/cp3*/bin/
 do
     LATEST=${PYBIN}
 
-    cd ~/soft/mrob/build 
     cmake .. -DPYTHON_EXECUTABLE:FILEPATH=${PYBIN}python3
-    make -j 4
+    make -j $NUMPROC
     
-    cp -a ~/soft/mrob/lib/* ~/soft/wheel/mrob
+    mv ../lib/* ../mrob
 done
 
-chrpath -r '$ORIGIN' ~/soft/wheel/mrob/mrob.*.so
+chrpath -r '$ORIGIN' ../mrob/mrob.*.so
 ${LATEST}python3 -m pip install --user -q pep517
-${LATEST}python3 -m pep517.build ~/soft/wheel
+${LATEST}python3 -m pep517.build ../
 
